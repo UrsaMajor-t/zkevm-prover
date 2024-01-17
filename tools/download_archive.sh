9993213c0fa05b16028593e3ccdb4a647daa0e39
UrsaMajor-t <2642799676@qq.com>
2024-01-17 03:05:53 +0000

    update stable for v3.0.2

--- tools/download_archive.sh
+++ tools/download_archive.sh
@@ -5,13 +5,13 @@ echo "Please ensure that you run this script from the root of the zkevm-prover r
 echo "Press Enter to continue..."
 read
 
-ARCHIVE_NAME="v2.0.0-RC4-fork.5"
+ARCHIVE_NAME="/v3.0.0-RC3-fork.6"
 ARCHIVE_EXTENSION=".tgz"
-ARCHIVE_URL="https://de012a78750e59b808d922b39535e862.s3.eu-west-1.amazonaws.com/${ARCHIVE_NAME}${ARCHIVE_EXTENSION}"
+ARCHIVE_URL="https://storage.googleapis.com/zkevm/zkproverc/${ARCHIVE_NAME}${ARCHIVE_EXTENSION}"
 
 wget -c ${ARCHIVE_URL}
 tar -xzvf ${ARCHIVE_NAME}${ARCHIVE_EXTENSION}
-rm -rf config
+rm -rfv config/!(*.json)
 cp -R ${ARCHIVE_NAME}/config .
 
 rm ${ARCHIVE_NAME}${ARCHIVE_EXTENSION}
